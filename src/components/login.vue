e8a9dbd24defe0a785a789c2076d39b9c2a9319c
LiLiang530 <1102411088@qq.com>
2021-12-24 14:59:31 +0800

    24

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,98 +1,138 @@
 <template>
-    <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-        <el-form-item label="用户名" prop="userName">
-            <el-input v-model="ruleForm.userName"></el-input>
-        </el-form-item>
-        <el-form-item label="密码" prop="password">
-            <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item>
-            <el-button type="primary" @click="login('ruleForm')">提交</el-button>
-            <el-button @click="resetForm('ruleForm')">重置</el-button>
-            <el-button type="primary" @click="toRegister()">注册</el-button>
-            <el-button type="danger" @click="toFindbackPassword()">找回密码</el-button>
-        </el-form-item>
+  <div id="a">
+    <!-- <img id="bg" src="../assets/bg.jpg"/> -->
+    <div id="b">
+    <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm">
+      <el-form-item label="用户名" prop="userName">
+        <el-input v-model="ruleForm.userName"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="login('ruleForm')" plain>提交</el-button>
+        <el-button @click="resetForm('ruleForm')" plain>重置</el-button>
+        <el-button type="primary" @click="toRegister()" plain>注册</el-button>
+        <el-button type="danger" @click="toFindbackPassword()" plain>找回密码</el-button>
+      </el-form-item>
     </el-form>
+    </div>
+  </div>
 </template>
+
 <script>
-  export default {
-    data() {
-      var checkName = (rule, value, callback) => {
-        if (value === '') {
-          setTimeout(() => {
-            callback(new Error('请输入用户名、(*￣3￣)╭'));
+export default {
+  data() {
+    var checkName = (rule, value, callback) => {
+      if (value === '') {
+        setTimeout(() => {
+          callback(new Error('请输入用户名、(*￣3￣)╭'));
         }, 1000);
-        }
-        else{
-          callback();
-        }
-      };
-      var validatePass = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('请输入密码'));
-        } else {
-          callback();
-        }
-      };
-      return {
-        ruleForm: {
-          password: '',
-          userName: ''
-        },
-        rules: {
-          password: [
-            { validator: validatePass, trigger: 'blur' }
-          ],
-          userName: [
-            { validator: checkName, trigger: 'blur' }
-          ]
-        }
-      };
-    },
-    methods: {
-      login(formName) {
-        this.$refs[formName].validate((valid) => {
-          // 验证成功发送请求
-          if (valid) {
-            this.$http({
-              method: 'post',
-              url: '/user/login',
-              data: {
-                  userName: this.ruleForm.userName,
-                  password: this.ruleForm.password
-              }
-            })
-            .then(({ data }) => {
-              // 保存Token和登录信息
-              console.log(data);
-              // 这里用了两种方式，localstore 和 vueX 分别 Token 和 userId
-              localStorage.setItem('Token',data.token)
-              this.$store.state.userId = data.userId
-              this.$store.commit('auth')
-              // console.log(this.$store.state.isAuth)
-              // console.log(this.$store.state.userId)
-               this.$router.push({path: '/userInfo'})
-            })
-            .catch(({ error }) => {
-              console.log("err");
-              console.log(error);
-            })
-          }
-          else {
-            console.log('error submit!!');
-            return false;
-          }
-        });
-      },
-      resetForm(formName) {
-        this.$refs[formName].resetFields();
-      },
-      toRegister(){
-        this.$router.push({path: '/register'});
-      },
-      toFindbackPassword(){
-        this.$router.push({path: '/findBackPwd'});
       }
+      else{
+        callback();
+      }
+    };
+    var validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'));
+      } else {
+        callback();
+      }
+    };
+    return {
+      ruleForm: {
+        password: '',
+        userName: ''
+      },
+      rules: {
+        password: [
+          { validator: validatePass, trigger: 'blur' }
+        ],
+        userName: [
+          { validator: checkName, trigger: 'blur' }
+        ]
+      }
+    };
+  },
+  methods: {
+    login(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.$http({
+            method: 'post',
+            url: '/user/login',
+            data: {
+              userName: this.ruleForm.userName,
+              password: this.ruleForm.password
+            }
+          })
+          .then(({ data }) => {
+            // 保存Token和登录信息
+            console.log(data)
+            if(data.state){
+              console.log("认证成功")
+              this.$store.commit('auth', data.userId, data.token)
+              this.$router.push({path: '/userInfo'})
+            } else {
+              console.log("失败")
+            }
+          })
+          .catch(({ error }) => {
+            console.log("err");
+            console.log(error);
+          })
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+    toRegister(){
+      this.$router.push({path: '/register'});
+    },
+    toFindbackPassword(){
+      this.$router.push({path: '/findBackPwd'});
     }
   }
+}
 </script>
+
+<style>
+#a{
+  height: 100%;
+  background-image: url('../assets/bg.jpg');
+  background-size: 100% 100%;
+}
+#b{
+  position: absolute;
+  background-color: rgba(255, 192, 203,.5);
+  border-radius: 10px;
+  height: 300px;
+  width: 400px;
+  padding: 20px 20px 0px 20px;
+  top: 50%;
+  left: 50%;
+  margin: -150px -200px;
+  
+}
+.el-button{
+  margin: 10px 10px;
+}
+.el-form-item__label{
+  font-size: 24px;
+}
+/* #bg{
+  position:absolute;
+  left:0px;
+  top:0px;
+  z-index:-1;
+  width: auto;
+  height: auto;
+  max-width: 100%;
+  max-height: 100%;
+} */
+</style>
--- src/components/welcome.vue
+++ src/components/welcome.vue
@@ -4,10 +4,7 @@
     <div>CSS布局待完善，记得把这两个div删掉</div>
     <img src="~@/assets/logo.png" />
     <div>
-      <el-button v-if="this.$store.state.isAuth == 0" @click="toLogin">Login</el-button>
-      <el-button v-else @click="toUserInfo">UserInfo</el-button>
       <el-button @click="toEditor">Editor</el-button>
-      <el-button @click="toNoteList">Notes</el-button><el-button @click="toComment">Comment</el-button>
     </div>
   </div>
 </template>
@@ -22,29 +19,9 @@ export default {
     }
   },
   methods:{
-    toLogin() {
-      //this.$router.push("/xx")
-      //传递的参数用{{ $route.query.xxx }}获取
-      this.$router.push({path: '/login'})
-      //后退两步
-      //this.$router.go(-2)
-    },
     toEditor() {
       this.$router.push({path: '/editor'})
     },
-    toUserInfo() {
-      this.$router.push({path:'/userInfo'})
-    },
-    toNoteList() {
-      if(this.$store.state.isAuth === 0){
-        this.$message('先登录哟(＾Ｕ＾)ノ~ＹＯ');
-      } else {
-        this.$router.push({path:'/noteList'})
-      }
-    },
-    toComment() {
-      this.$router.push({path:'/comments'})
-    }
   }
 }
 </script>
