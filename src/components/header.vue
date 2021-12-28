80352ff52d3b5801cea4853358d364b6b1560963
LiLiang530 <1102411088@qq.com>
2021-12-28 16:47:35 +0800

    didi

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="h">
     <el-menu class="header" mode="horizontal" router>
-      <el-menu-item index="/welcome"
-        ><img src="../assets/pic.jpg" />&emsp;codepen</el-menu-item
-      >
+      <el-menu-item index="/welcome">
+        <img src="../assets/pic.jpg" />&emsp;codepen
+      </el-menu-item>
       <el-menu-item index="/comments">评论区</el-menu-item>
-      <el-menu-item v-if="ifLogin" id="r0" index="/login"
-        ><img src="../assets/pic.jpg" />&emsp;请登录</el-menu-item
-      >
+      <el-menu-item v-if="ifLogin" id="r0" index="/login">
+        <img src="../assets/pic.jpg" />&emsp;请登录
+      </el-menu-item>
       <el-submenu v-else id="r1" index="null">
-        <template slot="title"
-          ><img src="../assets/pic.jpg" />&emsp;{{ userName }}</template
-        >
+        <template slot="title">
+          <img src="../assets/pic.jpg" />&emsp;{{ userName }}
+        </template>
         <el-menu-item index="/user-hub">个人主页</el-menu-item>
         <el-menu-item index="/welcome" @click="logout">登出</el-menu-item>
       </el-submenu>
